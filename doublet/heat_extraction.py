e6882d119654f9cac1a62d9b7f0bc7f5e00abb0c
Jan <jan.tecklenburg@gmx.de>
2025-09-22 12:38:44 +0200

    Adapt parameters to analytical solution

--- doublet/heat_extraction.py
+++ doublet/heat_extraction.py
@@ -5,7 +5,7 @@ except ModuleNotFoundError:
 
 rho = 1000.0      # kg/m³
 cp = 4180.0       # J/(kg·K)
-Q = 0.03          # m³/s
+Q = 0.03 / 2      # m³/s, halbes Gebiet!
 well_length = 30  # m 
 
 class HeatExtraction(OpenGeoSys.SourceTerm):
