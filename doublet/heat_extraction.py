8c1ece1908711e46a563d2dd569f17bdc4ca9e03
Jan <jan.tecklenburg@gmx.de>
2025-11-06 09:54:23 +0100

    well surface

--- doublet/heat_extraction.py
+++ doublet/heat_extraction.py
@@ -1,24 +1,36 @@
+# +
 try:
     import ogs.callbacks as OpenGeoSys
 except ModuleNotFoundError:
     import OpenGeoSys
 
+from parameter import Area
+
+# +
 rho = 1000.0      # kg/m³
 cp = 4180.0       # J/(kg·K)
 Q = 0.03 / 2      # m³/s, halbes Gebiet!
 well_length = 30  # m 
+
+print(Area)
+
+
+# -
 
 class HeatExtraction(OpenGeoSys.SourceTerm):
     def getFlux(self, t, coords, primary_vars):
         temperature = primary_vars[0]
         pressure = primary_vars[1]
 
-        print(f"T = {temperature}")
-        print(f"p = {pressure}")
+        #print(f"T = {temperature}")
+        #print(f"p = {pressure}")
 
         # Wärmefluss pro Länge (W/m)
-        q = -(rho * cp * Q * temperature) / well_length # W/m
-        dqdT = -(rho * cp * Q) / well_length
+        q = -(rho * cp * Q * temperature) / Area # W/m²
+        dqdT = -(rho * cp * Q) / Area
+        
+        #q = -(rho * cp * Q * temperature) / well_length # W/m
+        #dqdT = -(rho * cp * Q) / well_length
         
         return (q, [dqdT, 0.0])
 
